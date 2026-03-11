ffa72fc67a2df892d8e72968e68df2fb1abe9a61
acnk <2479303241@qq.com>
2026-03-11 15:23:24 +0800

    Update ACM solutions

--- 1.cpp
+++ 1.cpp
@@ -10,49 +10,10 @@ using ll = long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
  
-struct all{
-    int id;
-    int mon;
-    int cnt;
-};
-
-bool cmp(const all a,const all b){
-    if(a.mon != b.mon){
-        return a.mon > b.mon;
-    }
-    else if(a.cnt != b.cnt){
-        return a.cnt > b.cnt;
-    }
-    else a.id < b.id;
-}
-
-
 void pre(){} 
  
 void solve (){
-    int n; cin >> n;
-    vector<all>mp(n+1);
-    for(int i = 1 ; i <= n ; i++){
-        mp[i].id = i;
-        int k; cin >> k;
 
-        ll sum = 0;
-        while(k--){
-            int n,p; cin >> n >> p;
-            mp[n].mon += p;
-            sum += p;
-            mp[n].cnt++;
-        }
-        mp[i].mon -= sum;
-    }
-
-    sort(all(mp),cmp);
-
-    for(auto [x,y,z]:mp){
-        if(x == 0) continue;
-        double mon = y / 100.0;
-        printf("%d %.2lf\n",x,mon);
-    }
 }
  
 signed main () {
